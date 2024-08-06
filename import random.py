7996fb0c859660d1a4629bc363cea2ee7c882ea1
farah Mohamed <farah.m.eissa2005@gmail.com>
2024-08-06 23:38:11 +0300

    Add files via upload

---
+++ import random.py
@@ -0,0 +1,132 @@
+import random
+
+# List of words to choose from
+word_list = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+
+def hangman():
+    word = random.choice(word_list)
+    word_length = len(word)
+    display = ['_'] * word_length
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+
+    print("Let's play Hangman!")
+    print(display_hangman(tries))
+    print(' '.join(display))
+
+    while not guessed and tries > 0:
+        guess = input("Please guess a letter or word: ").lower()
+
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already guessed the letter", guess)
+            elif guess not in word:
+                print(guess, "is not in the word.")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Good job,", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word)
+                indices = [i for i, letter in enumerate(word_as_list) if letter == guess]
+                for index in indices:
+                    display[index] = guess
+        elif len(guess) == word_length and guess.isalpha():
+            if guess in guessed_words:
+                print("You already guessed the word", guess)
+            elif guess != word:
+                print(guess, "is not the word.")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                display = list(guess)
+        else:
+            print("Not a valid guess.")
+
+        print(display_hangman(tries))
+        print(' '.join(display))
+
+    if guessed:
+        print("Congratulations, you guessed the word!")
+    else:
+        print("Sorry, you ran out of tries. The word was " + word)
+
+def display_hangman(tries):
+    stages = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[tries]
+
+if __name__ == "__main__":
+    hangman()
